3df9853db6dfd604d085e4de224c46a051e3e92b
Paulo Henrique <zrick.gamer@gmail.com>
2025-03-18 19:34:53 -0300

    Update password_utils.py

--- register-py/utils/password_utils.py
+++ register-py/utils/password_utils.py
@@ -1,8 +1,11 @@
 import hashlib
+import base64
 
 def hash_password(password):
     """
     Hash a password using MD5 (for compatibility with the existing system)
     Note: MD5 is not secure for new systems, but we're using it for compatibility
     """
-    return hashlib.md5(password.encode()).hexdigest()
+    md5_hash = hashlib.md5(password.encode()).digest()
+    base64_hash = base64.b64encode(md5_hash).decode()
+    return base64_hash
